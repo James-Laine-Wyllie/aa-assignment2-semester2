7de1169f3369e4bfb9b4671d4e09c135e54cfcab
James Laine-Wyllie <james.lainewyllie@gmail.com>
2018-09-19 18:47:42 +1000

    Add print statements to follow code flow to understande code
    		- Work on initialisePlayer() method

--- src/player/RandomGuessPlayer.java
+++ src/player/RandomGuessPlayer.java
@@ -2,6 +2,7 @@ package player;
 
 import java.util.Scanner;
 import world.World;
+import java.util.*;
 
 /**
  * Random guess player (task A).
@@ -11,9 +12,57 @@ import world.World;
  */
 public class RandomGuessPlayer implements Player{
 
+    // attributes to keep track of ships, co-ordinates, and remaining ships
+
+    World world;
+
+    // these can be accesed via the world object: this.world.numRow
+    // convient varaible to keep track inside player
+
+    // to keep track of guesses made on board add combination of rows x column
+    // to a queue and pop
+    // ensures guess only made once, and random
+
+    int numRow;
+    int numColumn;
+    int gridSize;
+
+
+    int numberOfShipsRemaing;
+
+    // queue of grid locations
+
+    // initialisePlayer will set up the player ships, locations, and board
     @Override
     public void initialisePlayer(World world) {
         // To be implemented.
+
+        this.world = world;
+
+        System.out.println("initialisePlayer Test");
+        System.out.println("initialisePlayer: ");
+        System.out.println("World Information: ");
+        System.out.println("Number of ships: " + world.shipLocations.size());
+        // ship in ship co-ordinates has an associated ship and coordinate
+
+        for(int i = 0; i < world.shipLocations.size(); i++) {
+
+            System.out.println();
+            System.out.println("Ship information: ");
+            System.out.println("Ship: " + world.shipLocations.get(i).ship);
+            System.out.println("coordinates: " + world.shipLocations.get(i).coordinates.get(0));
+            System.out.println("------------------------------");
+
+        }
+
+
+        System.out.println("Size of grid: ");
+        System.out.println("rows: " + world.numRow);
+        System.out.println("colums: " + world.numColumn);
+
+
+        System.out.println("\nEnd of initialisePlayer()\n");
+
     } // end of initialisePlayer()
 
     @Override
