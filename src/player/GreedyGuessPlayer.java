fcaff96dfc8b9a8d55a96b353527d2f87e705ad4
James Laine-Wyllie <james.lainewyllie@gmail.com>
2018-10-02 00:27:45 +1000

    Creating attributes and constructor method

--- src/player/GreedyGuessPlayer.java
+++ src/player/GreedyGuessPlayer.java
@@ -9,11 +9,102 @@ import world.World;
  *
  * @author Youhan Xia, Jeffrey Chan
  */
+public enum Mode {
+
+    TARGETING,
+    HUNTING
+}
+
 public class GreedyGuessPlayer  implements Player{
+
+        // attributes to keep track of ships, co-ordinates, and remaining ships
+
+        World world;
+
+        // these can be accesed via the world object: this.world.numRow
+        // convient varaible to keep track inside player
+
+        // to keep track of guesses made on board add combination of rows x column
+        // to a queue and pop
+        // ensures guess only made once, and random
+        int numRow;
+        int numColumn;
+        int gridSize;
+
+        int numberOfShipsRemaing;
+
+        // keep track of which mode the player is in, defaut is TARGETING
+        Mode whichMode = TARGETING;
+
+        // want to keep track of all co-ordinates, parity co-ordinates, co-ordinates guessed
+        // neighbour co-ordinate of a correct guess will be in allCoordinates
+        // unless already guessed -- prevent repeat guesses
+
+        // Store all potential coordinates of grid
+        ArrayList<World.Coordinate> allCoordinates = new ArrayList<World.Coordinate>();
 
     @Override
     public void initialisePlayer(World world) {
-        // To be implemented.
+
+        this.world = world;
+
+        System.out.println("initialisePlayer Test");
+        System.out.println("initialisePlayer: ");
+        System.out.println("World Information: ");
+        System.out.println("Number of ships: " + world.shipLocations.size());
+        // ship in ship co-ordinates has an associated ship and coordinate
+
+        for(int i = 0; i < world.shipLocations.size(); i++) {
+
+            System.out.println();
+            System.out.println("Ship information: ");
+            System.out.println("Ship: " + world.shipLocations.get(i).ship);
+            System.out.println("coordinates: " + world.shipLocations.get(i).coordinates.get(0));
+            System.out.println("------------------------------");
+
+        }
+
+
+        System.out.println("Size of grid: ");
+        System.out.println("rows: " + world.numRow);
+        System.out.println("colums: " + world.numColumn);
+
+        // number of Initial ships
+        this.numberOfShipsRemaing = world.shipLocations.size();
+
+        // storing inforamtion about the board
+        this.numRow = world.numRow;
+        this.numColumn = world.numColumn;
+        this.gridSize = this.numRow * this.numColumn;
+
+        for(int row = 0; row < numRow; row++) {
+
+            for(int column = 0; column < numColumn; column++) {
+                // !!!!!!!!!! ---- ISSUE: how to create a co-ordinate outside world class ---->
+
+                // this works, not sure why
+                World.Coordinate newCoordinate = world.new Coordinate();
+
+                newCoordinate.row = row;
+                newCoordinate.column = column;
+
+                allCoordinates.add(newCoordinate);
+            }
+        }
+
+
+
+        for(int index = 0; index < allCoordinates.size(); index++) {
+
+            System.out.println("allCoordinates: element: " + index);
+            System.out.println(allCoordinates.get(index).toString());
+            System.out.println("Type of: " + allCoordinates.get(index).getClass().getName() + "\n");
+        }
+
+
+
+
+
     } // end of initialisePlayer()
 
     @Override
